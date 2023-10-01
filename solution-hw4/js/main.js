27c22c996fa09867c5c039dbfbfca562a80f2e5b
cassiex2 <yiwenx2@andrew.cmu.edu>
2023-10-01 15:35:00 -0400

    update js

--- solution-hw4/js/main.js
+++ solution-hw4/js/main.js
@@ -6,7 +6,6 @@ const glazingOptions = [
     {option: "Double chocolate", price: 1.5}
 ]
 
-
 // pack size object   
 const packSize = [
     {value: "1", price: 1},
@@ -43,6 +42,86 @@ for (i=0; i < packSize.length; i++) {
     currentPack.appendChild(createPack);
 
 }
+
+
+/* ---------  update price in cart -----------*/
+
+const cart = [];
+
+//update Gallary page
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const chosenRoll = params.get('roll')
+
+// Update the header text
+const headerElement = document.querySelector('#detail-roll-text');
+headerElement.innerText = chosenRoll + ' cinnamon roll'
+
+// Update the image
+const rollImage = document.querySelector('#product-image');
+rollImage.src = '../assets/products/' + chosenRoll.toLowerCase() + '-cinnamon-roll.jpg';
+
+// Update the price
+let basePrice = rolls[chosenRoll]['basePrice'];
+
+const totalPrice = document.querySelector('#cinnamon-roll-price');
+
+function priceChange() {
+
+    const glazingValue = document.getElementById('cinnamon-roll-price').value;
+    const packValue = document.getElementById('roll-pack-size').value;  
+    let glazingPrice = 0;
+    let packPrice = 0;
+
+    // find corresponding glazing price
+    for (i=0; i < glazingOptions.length; i++) {
+        if (glazingOptions[i].option === glazingValue) {
+            glazingPrice = glazingOptions[i].price;  
+        }
+        
+    }
+
+    // find corresponding price for n packs
+    for (i=0; i < packSize.length; i++) {
+        if (packSize[i].value === packValue) {
+            packPrice = packSize[i].price;  
+        }
+        
+    }
+
+    // calculate total roll price
+    const totalPrice = (glazingPrice + basePrice)*packPrice;
+
+    document.getElementById('total-price').innerHTML = `$ `+ totalPrice.toFixed(2);
+
+}
+
+priceChange();
+
+
+class Roll {
+    constructor(chosenRoll, rollGlazing, packSize, basePrice) {
+        this.type = chosenRoll;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+        
+    }
+}
+
+let addToCart = document.querySelector('.add-btn');
+addToCart.addEventListener("click", myFunction);
+
+function myFunction() {
+    const glazingValue = document.getElementById('cinnamon-roll-price').value;
+    const packValue = document.getElementById('roll-pack-size').value; 
+
+    myRoll = new Roll(chosenRoll, glazingValue,packValue,basePrice);
+    cart.push(myRoll)
+    console.log(cart);
+}
+
+
 
 
 
